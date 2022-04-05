f0a21defbfd24384b4f41badadd360cb917af682
Brian Arnold <b.arnold09@gmail.com>
2022-04-05 19:57:26 -0400

    commit tree dd1124d1e7baf5345a0683ce4f6306c43d69cc0b

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/students/:id', to: 'students#show'
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
